d63607d0fdd5cdc738df5d18c22e7a4a3c8ebb41
jaswanthyeddula <jaswanthyeddula31@gmail.com>
2026-06-09 08:17:16 +0530

    use SQLite for deployment

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 class Config:
-    SECRET_KEY = 'mysecertpass123'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'mysecertpass123')
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:jassu123@localhost/library_db'
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:jassu123@localhost/library_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///library.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'super-long-secret-key-1234567789') 
